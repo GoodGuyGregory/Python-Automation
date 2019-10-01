3c4766704d56ae8bc38487f6af79046e865c887f
GoodGuyGregory <greg.witt625@gmail.com>
2019-10-01 17:44:38 -0400

    initial commit

---
+++ Manipulating Strings/multilinecomments.py
@@ -0,0 +1,15 @@
+"""This is a test Python Program.
+Written by Me...
+
+This Program was designed for Python 3,
+"""
+
+
+def spam():
+    """This is a multiline comment to 
+    help explain what the spam() function does."""
+    print('Hello!')
+
+
+# Tests the Method
+spam()
