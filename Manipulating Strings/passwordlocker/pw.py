a7473bee6d21599bee1664daeba098e16d82b25e
GoodGuyGregory <greg.witt625@gmail.com>
2019-10-12 11:11:31 -0400

    starts dev on chapter prog  passwordlocker.py

---
+++ Manipulating Strings/passwordlocker/pw.py
@@ -0,0 +1,9 @@
+#! python3
+# pw.py - An inseure password locker program.
+
+# The dictionary wil be the data structure that organizes your account and password data.
+# simply call your account from the commandline to get the password for that specific account
+
+PASSWORDS = {
+
+}
