7b1a68ba18c148ed21acf1a18f96f9722910e7ac
GoodGuyGregory <greg.witt625@gmail.com>
2019-12-20 00:39:56 -0500

    adds changes to password program

--- Manipulating Strings/passwordlocker/pw.py
+++ Manipulating Strings/passwordlocker/pw.py
@@ -4,6 +4,14 @@
 # The dictionary wil be the data structure that organizes your account and password data.
 # simply call your account from the commandline to get the password for that specific account
 
-PASSWORDS = {
+import sys
+PASSWORDS = {'email': 'EMAILPASSWORD',
+             'blog': 'BLOGPASSWORD',
+             'luggage': '12345'
+             }
 
-}
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+
+account = sys.argv[1]  # first command line arg is the account name
