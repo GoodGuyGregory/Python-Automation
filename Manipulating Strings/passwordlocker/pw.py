d25c171689e8eb2a30c1595f0df0abfa92c88ea2
GoodGuyGregory <greg.witt625@gmail.com>
2019-12-21 19:52:16 -0500

    adds the password program

--- Manipulating Strings/passwordlocker/pw.py
+++ Manipulating Strings/passwordlocker/pw.py
@@ -4,7 +4,13 @@
 # The dictionary wil be the data structure that organizes your account and password data.
 # simply call your account from the commandline to get the password for that specific account
 
+# NOTES:
+# pyperclip module not workin in python3.xx
+# use python2.7 > python3.x in order to properly operate:
+
+
 import sys
+import pyperclip
 PASSWORDS = {'email': 'EMAILPASSWORD',
              'blog': 'BLOGPASSWORD',
              'luggage': '12345'
@@ -15,3 +21,9 @@ if len(sys.argv) < 2:
     sys.exit()
 
 account = sys.argv[1]  # first command line arg is the account name
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + 'copied to clipboard.')
+else:
+    print('There is no account named ' + account)
