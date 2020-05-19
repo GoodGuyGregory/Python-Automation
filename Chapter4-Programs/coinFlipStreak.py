75811d9903f8707525f20f316bf9329a21e842f1
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-19 15:57:57 -0400

    adds segmented lists for searching

--- Chapter4-Programs/coinFlipStreak.py
+++ Chapter4-Programs/coinFlipStreak.py
@@ -2,23 +2,42 @@ import random
 numberOfStreaks = 0
 
 
-def generateTrails():
-    trialList = []
-    for i in range(100):
-        flipResult = random.randint(0, 1)
-        if flipResult == 1:
-            print("Got HEADS")
-            trialList.append('H')
-        elif flipResult == 0:
-            print("Got TAILS")
-            trialList.append('T')
+def main():
+    for experimentNumber in range(100):
+        # Code that creates a list of 100 'heads' or 'tails' values.
+        print()
+        print("Running Trail " + str(experimentNumber))
+        trialList = []
+        print("=================================")
+
+        for i in range(100):
+            print("Flipping Coin....")
+            flipResult = random.randint(0, 1)
+            if flipResult == 1:
+                print("Got HEADS")
+                trialList.append('H')
+            elif flipResult == 0:
+                print("Got TAILS")
+                trialList.append('T')
+
+        #  slice list into 6 elements of the sublist and compare with
+        for i in range(len(trialList)):
+            headStreak = 0
+            segmentList = str(trialList[i:i+6])
+            print(segmentList)
+            if segmentList == str(['H', 'H', 'H', 'H', 'H', 'H']):
+                headStreak += 1
+                print(headStreak)
+        for i in range(len(trialList)):
+            tailStreak = 0
+            segmentList = trialList[i:6]
+            if segmentList == str(['T', 'T', 'T', 'T', 'T', 'T']):
+                tailStreak += 1
+
+            numberOfStreaks = headStreak + tailStreak
+            # Code that checks if there is a streak of 6 heads or tails in a row.
+
+    print('Chance of streak: %s%%' % (numberOfStreaks / 100))
 
 
-for experimentNumber in range(10000):
-    # Code that creates a list of 100 'heads' or 'tails' values.
-    print()
-    print()
-    print("Running Trails")
-    generateTrails()
-    # Code that checks if there is a streak of 6 heads or tails in a row.
-print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+main()
