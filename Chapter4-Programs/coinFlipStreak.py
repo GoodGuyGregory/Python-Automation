9fce44edd0428a3beafacc980f94b1ee83de62ad
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-19 22:26:13 -0400

    adds working coinFlip

--- Chapter4-Programs/coinFlipStreak.py
+++ Chapter4-Programs/coinFlipStreak.py
@@ -3,7 +3,9 @@ numberOfStreaks = 0
 
 
 def main():
-    for experimentNumber in range(100):
+    headStreak = 0
+    tailStreak = 0
+    for experimentNumber in range(10000):
         # Code that creates a list of 100 'heads' or 'tails' values.
         print()
         print("Running Trail " + str(experimentNumber))
@@ -11,33 +13,30 @@ def main():
         print("=================================")
 
         for i in range(100):
-            print("Flipping Coin....")
+            # print("Flipping Coin....")
             flipResult = random.randint(0, 1)
             if flipResult == 1:
-                print("Got HEADS")
+                # print("Got HEADS")
                 trialList.append('H')
             elif flipResult == 0:
-                print("Got TAILS")
+                # print("Got TAILS")
                 trialList.append('T')
 
         #  slice list into 6 elements of the sublist and compare with
         for i in range(len(trialList)):
-            headStreak = 0
             segmentList = str(trialList[i:i+6])
-            print(segmentList)
+            # print(segmentList)
             if segmentList == str(['H', 'H', 'H', 'H', 'H', 'H']):
                 headStreak += 1
-                print(headStreak)
-        for i in range(len(trialList)):
-            tailStreak = 0
-            segmentList = trialList[i:6]
-            if segmentList == str(['T', 'T', 'T', 'T', 'T', 'T']):
+            elif segmentList == str(['T', 'T', 'T', 'T', 'T', 'T']):
+                # print(tailStreak)
                 tailStreak += 1
+    print("Overall Head Streaks: " + str(headStreak))
+    print("Overall Tail Streaks: " + str(tailStreak))
+    numberOfStreaks = headStreak + tailStreak
+    # Code that checks if there is a streak of 6 heads or tails in a row.
 
-            numberOfStreaks = headStreak + tailStreak
-            # Code that checks if there is a streak of 6 heads or tails in a row.
-
-    print('Chance of streak: %s%%' % (numberOfStreaks / 100))
+    print('Chance of streak: %s%%' % (numberOfStreaks / 10000))
 
 
 main()
