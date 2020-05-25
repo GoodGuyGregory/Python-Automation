ea2305c02deccf356d8743a751cbaabd617d145a
GoodGuyGregory <greg.witt625@gmail.com>
2020-05-24 23:07:12 -0400

    inital listToDictionary commit

---
+++ Chapter5-Dictionaries/listToDictionary.py
@@ -0,0 +1,24 @@
+
+# def addToInventory(inventory, addedItems):
+
+
+def displayInventory(playerInventory):
+    print('Inventory:')
+    itemCount = 0
+    for k, v in playerInventory.items():
+        print(str(v) + ' ' + str(k))
+        itemCount += v
+    print('Total number of items: ' + str(itemCount))
+
+
+def main():
+    # inventory example:
+    inv = {'gold coin': 42, 'rope': 1}
+
+    # example loot
+    dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+    displayInventory(inv)
+
+
+main()
