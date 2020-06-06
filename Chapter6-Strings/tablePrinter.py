c619ccde2cedaacc8bce29d91aa1aca2aa507281
GoodGuyGregory <greg.witt625@gmail.com>
2020-06-06 09:09:44 -0400

    adds tablePrint

--- Chapter6-Strings/tablePrinter.py
+++ Chapter6-Strings/tablePrinter.py
@@ -1,10 +1,13 @@
-def printTable():
+def printTable(tableData):
+    for i in range(len(tableData)):
+        print(tableData[i])
 
 
 def main():
     tableData = [['apples', 'oranges', 'cherries', 'banana'],
                  ['Alice', 'Bob', 'Carol', 'David'],
                  ['dogs', 'cats', 'moose', 'goose']]
+    printTable(tableData)
 
 
 main()
