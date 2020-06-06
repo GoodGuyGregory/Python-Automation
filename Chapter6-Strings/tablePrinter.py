9b1e0a3f24f35e8218465ce883f58491059054e6
GoodGuyGregory <greg.witt625@gmail.com>
2020-06-06 00:00:01 -0400

    initial commit for tablePrinter

---
+++ Chapter6-Strings/tablePrinter.py
@@ -0,0 +1,10 @@
+def printTable():
+
+
+def main():
+    tableData = [['apples', 'oranges', 'cherries', 'banana'],
+                 ['Alice', 'Bob', 'Carol', 'David'],
+                 ['dogs', 'cats', 'moose', 'goose']]
+
+
+main()
