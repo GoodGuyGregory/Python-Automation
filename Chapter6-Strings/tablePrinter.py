29d80025dbdd379343d54caf2819b286328073f8
GoodGuyGregory <greg.witt625@gmail.com>
2020-06-07 16:23:39 -0400

    adds iteration logic for tablePrinter

--- Chapter6-Strings/tablePrinter.py
+++ Chapter6-Strings/tablePrinter.py
@@ -1,12 +1,19 @@
 def printTable(tableData):
+    # creates an empty list for the widths for justificiation
+    colWidths = [0] * len(tableData)
+
+    # determine longest character in each nested list
     for i in range(len(tableData)):
-        print(tableData[i])
+        for j in range(len(tableData[i])):
+            # compare contents of the list at i
+            largest = tableData[i][j]
 
 
 def main():
     tableData = [['apples', 'oranges', 'cherries', 'banana'],
                  ['Alice', 'Bob', 'Carol', 'David'],
                  ['dogs', 'cats', 'moose', 'goose']]
+
     printTable(tableData)
 
 
