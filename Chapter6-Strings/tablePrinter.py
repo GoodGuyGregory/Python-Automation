fb9c5cadc43f5d4132aa8a8fd526dab5b8c99be0
GoodGuyGregory <greg.witt625@gmail.com>
2020-06-08 09:20:22 -0400

    adds colWidth calculations

--- Chapter6-Strings/tablePrinter.py
+++ Chapter6-Strings/tablePrinter.py
@@ -1,12 +1,24 @@
 def printTable(tableData):
     # creates an empty list for the widths for justificiation
     colWidths = [0] * len(tableData)
+    largest = ''
+    listCharFound = ''
 
-    # determine longest character in each nested list
+# determine longest character in each nested list
     for i in range(len(tableData)):
         for j in range(len(tableData[i])):
             # compare contents of the list at i
-            largest = tableData[i][j]
+            listCharFound = tableData[i][j]
+            print(listCharFound)
+            if len(listCharFound) > len(largest):
+                #  change the largest found
+                largest = listCharFound
+                # print(largest)
+            #  set length for colWidths to that value
+        colWidths[i] = len(largest)
+        print(str(colWidths[i]))
+        # clear the largest value
+        largest = ''
 
 
 def main():
