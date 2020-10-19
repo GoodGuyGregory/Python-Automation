9892e0a86e76e90f79a4fb16c8e65a586cb75e77
GoodGuyGregory <greg.witt625@gmail.com>
2020-10-19 00:39:15 -0400

    adds additional regex for additional testing

--- Chapter7-Regex/SecondEditionProject/strongPasswordDetection.py
+++ Chapter7-Regex/SecondEditionProject/strongPasswordDetection.py
@@ -1,6 +1,6 @@
 #! /Users/user/.pyenv/versions/3.7.5/bin/python3
 
-# strongPasswordDectection: checks whether 
+# strongPasswordDectection: checks whether
 # a copied password or entered password is strong.
 
 import re, pyperclip
@@ -9,25 +9,32 @@ import re, pyperclip
 
 def main():
     #  TODO:
-    #  build a function that uses REGEX to make sure the password string it is passed 
+    #  build a function that uses REGEX to make sure the password string it is passed
     #  is at least 8 characters long
     #  contains both upper and lower case letters
     #  has at least one digit
-    
-    
-    passwordRegex = re.compile()
-    
-    
+
     def strongPasswordChecker():
-         print("checking criteria")
-    
-    # Check input from the user:
-    if len(pyperclip.paste() == 0):
-         passwordToCheck = input('please type your password: ')
-         strongPasswordChecker(passwordToCheck)
-    else:
-        passwordToCheck = str(pyperclip.paste())
-        strongPasswordChecker(passwordToCheck)
-   
-        
+
+        # Password REGEXs
+        #  is at least 8 characters long
+        CharCountRegex = re.compile(r'.{8,}')
+        #  contains both upper and lower case letters
+        UpperLettersRegex = re.compile(r'[A-Z]')
+        #  has at least one digit
+        DigitRegex = re.compile(r'[0-9]')
+
+         print("checking criteria...")
+
+        # Check input from the user:
+        passwordToCheck = pyperclip.paste()
+        mo1 = passwordRegex.search(passwordToCheck);
+        mo1.group()
+        if mo1 === true:
+            print("Great Password")
+        else:
+            print("You can make a Better Password")
+            print("Make it at least Eight Characters for Starters")
+
+
 main()
