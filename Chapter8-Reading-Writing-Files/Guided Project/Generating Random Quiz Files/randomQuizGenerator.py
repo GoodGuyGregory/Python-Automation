e7397b507120b678d818c134e786d98e78098d18
GoodGuyGregory <greg.witt625@gmail.com>
2020-12-24 18:41:26 -0500

    adds Guided Projects Directory

---
+++ Chapter8-Reading-Writing-Files/Guided Project/Generating Random Quiz Files/randomQuizGenerator.py
@@ -0,0 +1,13 @@
+#! python3
+#  randomQuizGenerator.py - creates quizzes with questions and answers in
+# random order, along with the answer key.
+
+import random
+
+capitals = {'Alabama': 'Montgomery',
+            'Alaska': 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock',
+            'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford',
+            'Delaware': 'Dover', 'Florida': 'Tallahassee', 'Georgia': 'Atlanta',
+            'Hawaii': 'Honolulu', 'Idaho': 'Des Moines', 'Illinois': 'Springfield', 'Indiana': 'Indianapolis',
+            'Iowa': 'Des Moines', 'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
+            }
