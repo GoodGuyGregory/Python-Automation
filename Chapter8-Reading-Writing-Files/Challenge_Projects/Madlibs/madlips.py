50c2ccbdde92d69f50f36ce945127815724befc1
GoodGuyGregory <greg.witt625@gmail.com>
2021-02-04 13:55:35 -0500

    adds write to solutions file functionality

--- Chapter8-Reading-Writing-Files/Challenge_Projects/Madlibs/madlips.py
+++ Chapter8-Reading-Writing-Files/Challenge_Projects/Madlibs/madlips.py
@@ -3,6 +3,8 @@ import re
 
 def main():
     madlibs = open('madlib.txt', 'r')
+    # write to new
+    finishedMadlib = open('finishedMadlib.txt', 'w')
 
     # read the words:
     madlib = madlibs.readline()
@@ -15,7 +17,22 @@ def main():
         if words[i].isupper() and len(words[i]) >= 4 or words[i] in replacements:
             # creates a clean substring by removing punctuations
             cleanWord = re.sub(r'[^\w\s]', '', words[i])
-            print(cleanWord)
+            if cleanWord[0].lower() == 'a':
+                userWord = input("Enter an {}: " .format(cleanWord.lower()))
+                # swap the word
+                words[i] = userWord
+
+                finishedMadlib.write(str(words[i]) + " ")
+
+            else:
+                userWord = input("Enter a {}: " .format(cleanWord.lower()))
+                words[i] = userWord
+
+                finishedMadlib.write(str(words[i]) + " ")
+        else:
+            finishedMadlib.write(str(words[i]) + " ")
+    finishedMadlib.close()
+    madlibs.close()
 
 
 main()
