8e09852e0d21f3837ab228288806384e3d50991d
GoodGuyGregory <greg.witt625@gmail.com>
2021-02-14 21:17:24 -0500

    adds init commit for selective copy

---
+++ Chapter9-Organizing/Practice-Projects/Selective-Copy/selectiveCopy.py
@@ -0,0 +1,14 @@
+#! python3
+# selectiveCopy - a program designed to allow for a specified selection file type
+#  to be copied and added to a new folder
+
+
+def selectivecopy():
+
+
+def main():
+
+    selectivecopy()
+
+
+main()
