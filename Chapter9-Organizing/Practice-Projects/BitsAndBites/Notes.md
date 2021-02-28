df8d69a1be1a37cfc78c3df1da54425f44b8f642
GoodGuyGregory <greg.witt625@gmail.com>
2021-02-27 20:40:34 -0500

    adds Chapter9 and Bits and Bytes notes

---
+++ Chapter9-Organizing/Practice-Projects/BitsAndBites/Notes.md
@@ -0,0 +1,3 @@
+## Bits and Bytes:
+
+[Concept Documentation](https://www.youtube.com/watch?v=HRmfXA4EUBs)
--- Chapter9-Organizing/Practice-Projects/Delete-Unneeded-Files/deleteUnneededFiles.py
+++ Chapter9-Organizing/Practice-Projects/Delete-Unneeded-Files/deleteUnneededFiles.py
@@ -1,24 +1,52 @@
 import os
 
 
-def deleteUnneededFiles():
-    for folderName, subFolderNames, filenames in os.walk(os.:
+def deleteUnneededFiles(suppliedPath):
+    # change directory
+    os.chdir(suppliedPath)
+
+    deletedFiles = []
+
+    for folderName, subFolderNames, filenames in os.walk(os.getcwd()):
         print('The current folder is ' + folderName)
 
     # if there are iterables of multiple subfolders
         for subFolder in subFolderNames:
-            print('SUBFOLDER OF ' + folderName + ": " + subFolder)
+            print('Searching Subfolder of ' + folderName + ": " + subFolder)
 
             for filename in filenames:
-                print('FILE INSIDE ' + folderName + ': ' + filename)
-                jpegRegex = re.compile(r'.jpg$')
-                foundRegex = jpegRegex.search(filename)
-                if foundRegex:
-                    print("found %s" % filename)
+                pathForFile = os.path.join(suppliedPath, folderName, filename)
+                if os.path.getsize(pathForFile) > 100000000:
+                    deletedFiles.append(filename)
+                    print(filename + 'is larger than 100mb')
+
+        #  if there are no folders in the directory
+        for filename in filenames:
+            pathForFile = os.path.join(suppliedPath, folderName, filename)
+            if os.path.getsize(pathForFile) > 100000000:
+                deletedFiles.append(filename)
+                print(filename + 'is larger than 100mb')
+
+        #  display findings
+        print('===============================')
+        print('found ' + str(len(deletedFiles)) + ' file(s)')
+        print()
+        response = input('Do you want to delete them? [Y/n] ')
+        if response.lower() == 'y':
+            for file in deletedFiles:
+                print('deleting ' + file)
+        else:
+            print()
+            print('keeping these files')
+            print('exiting...')
 
 
 def main():
-    deleteUnneededFiles
+    # directoryToSearch = input('enter a directory to search (pwd): ')
+
+    directoryToSearch = '/Users/user/Desktop'
+
+    deleteUnneededFiles(directoryToSearch)
 
 
 main()
