7f17a636624a9a39c1d8f8a60472c72e1583328c
GoodGuyGregory <greg.witt625@gmail.com>
2021-11-18 10:39:58 -0500

    adds notes and stream capture with error handling

--- After-Text/yt-tools/copydownloadyt.py
+++ After-Text/yt-tools/copydownloadyt.py
@@ -1,15 +1,38 @@
 import pyperclip
 import re
 from pytube import YouTube
+from pytube.exceptions import RegexMatchError
 
 
 def downloadVideoFromClipboard():
+    # extract text from the clipboard
     clipboardText = pyperclip.paste()
     ytLinkRegex = re.compile(r'(https://www.youtube.com/\S+youtu.be)')
     listOfLinks = ytLinkRegex.findall(clipboardText)
-
+    # extract urls from content
     for i in range(len(listOfLinks)):
-        print(listOfLinks[i])
+        # print(listOfLinks[i])
+        ytVideoLink = listOfLinks[i]
+        # interpolate the yt object with string values
+        try:
+            desiredVideo = YouTube(ytVideoLink)
+        except RegexMatchError:
+            print('An RegexMatchError Exception Occured While Parsing The Clipboard Text')
+            print(f'{ytVideoLink} appears to have formatting issues')
+            if i <= len(listOfLinks):
+                print('skipping this entry...')
+                print('processing the remaining items found...')
+            else:
+                print(f'skipping link {ytVideoLink}')
+        except Exception as e:
+            print(f'An {e} Exception Occured While Parsing The Clipboard Text')
+            print(f'{ytVideoLink} appears to have formatting issues')
+        else:
+            # # establish a stream selection by iTag
+            # stream = sextonVideo.streams.get_by_itag(22)
+            # # download the selection
+            # stream.download('./')
+            print(desiredVideo.streams.filter(res="720p")[0])
 
 
 downloadVideoFromClipboard()
---
+++ After-Text/yt-tools/notes.md
@@ -0,0 +1,30 @@
+* [Exception Handling with Try Catch](https://www.poftut.com/python-try-catch-exceptions-tutorial/)
+
+Basic format for an exception 
+
+```python 
+#!/usr/bin/python3 
+
+num1 = input ("Enter number") 
+
+try:   print(num1*num1) 
+
+except TypeError:   
+   print("An TypeError occurred") 
+
+else: 
+   print("There is no exception")
+
+print("Operation completed")
+```
+
+More detailed examples from the python docs [Handling-Exceptions](https://docs.python.org/3/tutorial/errors.html#handling-exceptions)
+
+```python 
+
+
+```
+
+**String Format Printing**
+
+[String Print(f'{variable}')](https://www.programiz.com/python-programming/string-interpolation)
